e87d3c28fcc78c6b96f9db4bac868492d6839726
Ardhi <ardhi333@gmail.com>
2024-04-10 21:52:51 +0700

    Desgin Back End API for Sign In

--- server/controllers/auth.contoller.js
+++ server/controllers/auth.contoller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -27,6 +28,42 @@ export const signup = async (req, res, next) => {
     const data = await newUser.save();
     console.log('success input data');
     return res.json({ message: 'SignUp successfully' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const singin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === '' || password === '') {
+    next(errorHandler(400, 'All fields are required'));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      next(errorHandler(404, 'User not found!'));
+    }
+
+    const valiPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!valiPassword) {
+      next(errorHandler(400, 'Invalid Password'));
+    }
+
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+      },
+      process.env.JWT_SECRET
+    );
+
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie('access_token', token, { httpOnly: true })
+      .json(rest);
   } catch (error) {
     next(error);
   }
